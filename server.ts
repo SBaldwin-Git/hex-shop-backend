98aed0da439d8b06dbeca5759a4ec1827c5892fd
Sam Baldwin <sambaldwin92@gmail.com>
2024-04-03 15:42:53 +0100

    Backend application connects to database correctly now using typescript and modules

--- server.ts
+++ server.ts
@@ -1,6 +1,18 @@
-const express = require('express');
+import express from "express";
+import { pool } from "./src/db/connection";
+
 const app = express();
 
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.send(`Hello World! Server time: ${result.rows[0].now}`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error executing query");
+  }
+});
+
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log("Server is running on http://localhost:3000");
 });
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,8 +1,14 @@
-import { Pool } from "pg";
+//
+//
+// USE THIS npx tsx server.ts
+//
+//
+import pg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
 const connectionString = process.env.DB_CONNECTION_STRING;
+const Pool = pg.Pool;
 
 if (!connectionString) {
   throw new Error("DB_CONNECTION_STRING is not set in .env file");
@@ -12,4 +18,7 @@ export const pool = new Pool({
   connectionString,
 });
 
-await pool.connect();
+await pool
+  .connect()
+  .then(() => console.log("Connected to the database successfully"))
+  .catch((err) => console.error("Unable to connect to the database:", err));
